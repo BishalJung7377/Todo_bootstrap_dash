4bdc94c1a25976b7dd071f5bb96dd8125aafaba4
bsalt <bsalt@pop-os.localdomain>
2021-11-11 11:46:05 +0545

    Error: Validator error on password form

--- src/app/signup-password/signup-password.component.ts
+++ src/app/signup-password/signup-password.component.ts
@@ -1,4 +1,21 @@
+import { Router } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
+import {
+  FormGroup,
+  FormBuilder,
+  Validators,
+  AbstractControl,
+  ValidationErrors,
+  ValidatorFn,
+} from '@angular/forms';
+import { SignupserviceService } from 'src/app/services/SignupApi/signupservice.service';
+import {
+  passwordupperCase,
+  specialeChars,
+  numericPass,
+  Validateconfirmpassword,
+} from '../../Validator/validate';
+import { SignupUserData } from 'src/app/signup/SignUpUserData';
 
 @Component({
   selector: 'app-signup-password',
@@ -7,9 +24,70 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SignupPasswordComponent implements OnInit {
 
-  constructor() { }
+  title = 'TODO APP';
+  signupdata: SignupUserData = new SignupUserData();
+  PasswordForm!: FormGroup;
+  logintext = 'Login';
+  hide = true;
+  hidenew = true;
+  submit = false;
+  newpassword!: string;
+  cpassword!: string;
+  emailSent = 'Email has been sent!';
+  resetTxt =
+    'Please check your inbox and click in the recieved link to reset password';
+
+  constructor(
+    private formBuilder: FormBuilder,
+    public signupAuth: SignupserviceService,
+    public router: Router
+  ) {}
 
   ngOnInit(): void {
+    this.passsworfFormView();
+  }
+  passsworfFormView(): void {
+    this.PasswordForm = this.formBuilder.group(
+      {
+        newpassword: [
+          '',
+          [
+            Validators.required,
+            Validators.minLength(8),
+            passwordupperCase(),
+            specialeChars(),
+            numericPass(),
+          ],
+        ],
+        cpassword: ['', [Validators.required]],
+      },
+      { validators: Validateconfirmpassword() }
+    );
+  }
+
+  get nepassword() {
+    return this.PasswordForm.get('newpassword');
+  }
+
+  get confirmpassword() {
+    return this.PasswordForm.get('cpassword');
+  }
+
+  onSubmit() {
+    if (this.PasswordForm.valid) {
+      this.signupdata = this.signupAuth.userData;
+      this.signupdata.name = this.signupdata.name;
+      this.signupdata.email = this.signupdata.email;
+      this.signupdata.date = this.signupdata.date;
+      this.signupdata.phone = this.signupdata.phone;
+      this.signupdata.password = this.PasswordForm.value.cpassword;
+      this.signupdata.gender = this.signupdata.gender;
+      this.signupAuth.userSignUp(this.signupdata).subscribe((response) => {
+        console.log(response);
+      });
+    } else {
+      console.warn('Error User');
+    }
   }
 
 }
