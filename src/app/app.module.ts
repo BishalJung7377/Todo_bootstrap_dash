845de8d690409dc7a88f8b8a94c46183db65f02f
bsalt <bsalt@pop-os.localdomain>
2021-11-11 11:49:16 +0545

    Feat: forgot password added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -33,6 +33,7 @@ import { DashboardContentsComponent } from './dashboard/dashboard-contents/dashb
 import { SignupComponent } from './signup/signup.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { SignupPasswordComponent } from './signup-password/signup-password.component';
+import { ForgotPasswordComponent } from './forgot-password/forgot-password.component';
 
 @NgModule({
   declarations: [
@@ -44,7 +45,8 @@ import { SignupPasswordComponent } from './signup-password/signup-password.compo
     LoginComponent,
     DashboardContentsComponent,
     SignupComponent,
-    SignupPasswordComponent
+    SignupPasswordComponent,
+    ForgotPasswordComponent
   ],
   imports: [
     BrowserModule,
