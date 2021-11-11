04bfb1a80b52fae65ba18fbff8f1d92d61890e42
bsalt <bsalt@pop-os.localdomain>
2021-11-11 11:59:39 +0545

    Feat: logic added for opening, closing of modal

--- src/app/Modal/add-task/add-task.component.ts
+++ src/app/Modal/add-task/add-task.component.ts
@@ -1,4 +1,7 @@
+import { ListService } from './../../services/ListServices/list.service';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { Component, OnInit } from '@angular/core';
+import { MdbModalRef } from 'mdb-angular-ui-kit/modal';
 
 @Component({
   selector: 'app-add-task',
@@ -6,10 +9,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./add-task.component.scss']
 })
 export class AddTaskComponent implements OnInit {
+  newListtask!: FormGroup;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(
+    public modalRef: MdbModalRef<AddTaskComponent>,
+    private formBuilder: FormBuilder,
+    private modalAuth: ListService
+  ) {
+    this.newListtask = this.formBuilder.group({
+      ListName: ['', [Validators.required]],
+    });
   }
 
+  ngOnInit(): void {}
+  onSubmit() {
+    if (this.newListtask.valid) {
+      this.modalAuth
+        .createList(this.newListtask.value.ListName)
+        .subscribe((response) => {
+          console.log(response);
+        });
+    } else {
+      console.log('Error');
+    }
+  }
 }
