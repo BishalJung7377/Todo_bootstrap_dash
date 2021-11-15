58d65695518b13e6e4df6da7034d655888cfceda
Bishal Jung Thapa <bishaljung63@gmail.com>
2021-11-15 09:43:46 +0545

    Feat: logout function added

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,5 +1,6 @@
+import { Route } from '@angular/compiler/src/core';
 import { Component, OnInit } from '@angular/core';
-
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
@@ -7,9 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) {}
+
 
   ngOnInit(): void {
   }
-
+  logout() {
+    localStorage.clear();
+    this.router.navigate(['/']);
+  }
 }
