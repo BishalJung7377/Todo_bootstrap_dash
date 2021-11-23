3d343cf4f1aabb5d1bca858980bf1464d2302ace
Bishal Jung Thapa <bishaljung63@gmail.com>
2021-11-23 17:28:22 +0545

    Feat: Loading spinner created

--- src/app/services/interceptor_service.service.ts
+++ src/app/services/interceptor_service.service.ts
@@ -8,21 +8,24 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { finalize } from 'rxjs/operators';
 import { APIServiceService } from './api_service.service';
+import { LoaderService } from './loader/loader.service';
 
 @Injectable({
   providedIn: 'root',
 })
 export class InterceptorServiceService implements HttpInterceptor {
-  constructor(public loaderService: APIServiceService) {}
+  constructor(public loaderService: APIServiceService,
+    public itemLoaderService: LoaderService) {}
   intercept(
     req: HttpRequest<string>,
     next: HttpHandler
   ): Observable<HttpEvent<string>> {
     this.loaderService.isLoading.next(true);
-
+    this.itemLoaderService.isLoading.next(true);
     return next.handle(req).pipe(
       finalize(() => {
         this.loaderService.isLoading.next(false);
+        this.itemLoaderService.isLoading.next(false);
       })
     );
   }
